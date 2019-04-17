50fe063cd927338e685ffa820645e60d38c9392e
Søren Horsbøll Hansen (PC) <soren@horsboll.dk>
2019-04-17 14:33:59 +0200

    Nyt website
    Upload og rokering af filer til aktivering af bryllupswebsite

--- bryllup/assets/js/script.js
+++ bryllup/assets/js/script.js
@@ -1,4 +1,20 @@
+//@prepros-prepend vendor/in-view.js
+
 // Document ready
 $(function() {
-	
+	$("body").addClass('js');
+
+	$(".mainnav-toggle, #mainnav a").on("click", function() {
+		$("#mainnav--wrapper, .hamburger").toggleClass("is-active");
+	});
+
+
+	inView("h1, h2, h3, p, .section-label, .time, .icon svg, .cover-img, .animate")
+	.on("enter", function(el) {
+		$(el).addClass("in-view");
+	})
+	// .on('exit', el => {
+	// 	el.style.opacity = 0.5;
+	// });
+
 });
